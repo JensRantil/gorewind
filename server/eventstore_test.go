ccb16a7cbb9720874541470a5c53c1bbb97b1485
Jens Rantil <jens.rantil@gmail.com>
2013-03-13 08:00:33 +0100

    Minor: Name change: compareTest => comparatorTest

--- server/eventstore_test.go
+++ server/eventstore_test.go
@@ -23,20 +23,20 @@ import (
 )
 
 
-type compareTest struct {
+type comparatorTest struct {
 	a string
 	b string
 	expectedResult int
 }
 
-func testComparater(t *testing.T, test compareTest, c EventStreamComparer) {
+func testComparater(t *testing.T, test comparatorTest, c EventStreamComparer) {
 	testComparator(t, test, c)
 	testSeparator(t, test, c)
 	testSuccessor(t, test.a, c)
 	testSuccessor(t, test.b, c)
 }
 
-func testSeparator(t *testing.T, test compareTest, c EventStreamComparer) {
+func testSeparator(t *testing.T, test comparatorTest, c EventStreamComparer) {
 	ba := []byte(test.a)
 	bb := []byte(test.b)
 	res := c.Separator(ba, bb)
@@ -57,7 +57,7 @@ func testSeparator(t *testing.T, test compareTest, c EventStreamComparer) {
 	}
 }
 
-func testComparator(t *testing.T, test compareTest, comparer EventStreamComparer) {
+func testComparator(t *testing.T, test comparatorTest, comparer EventStreamComparer) {
 	res := comparer.Compare([]byte(test.a), []byte(test.b))
 	if res != test.expectedResult {
 		t.Errorf("a: %s", test.a)
@@ -80,31 +80,31 @@ func testSuccessor(t *testing.T, s string, comparer EventStreamComparer) {
 
 func TestComparator(t *testing.T) {
 	comparer := EventStreamComparer{}
-	tests := []compareTest{
-		compareTest{"g:a", "g:a", 0},
-		compareTest{"g:a", "g:b", -1},
-		compareTest{"g:a", "h:a", -1},
-		compareTest{"g:a:1", "g:a:1", 0},
-		compareTest{"g:a:1", "h:a:1", -1},
-		compareTest{"g:a:1", "h:a:2", -1},
-		compareTest{"g:a:1", "h:a:11", -1},
-		compareTest{"g:a:b:1", "g:a:b:11", -1},
+	tests := []comparatorTest{
+		comparatorTest{"g:a", "g:a", 0},
+		comparatorTest{"g:a", "g:b", -1},
+		comparatorTest{"g:a", "h:a", -1},
+		comparatorTest{"g:a:1", "g:a:1", 0},
+		comparatorTest{"g:a:1", "h:a:1", -1},
+		comparatorTest{"g:a:1", "h:a:2", -1},
+		comparatorTest{"g:a:1", "h:a:11", -1},
+		comparatorTest{"g:a:b:1", "g:a:b:11", -1},
 
-		// Test cases where there are multiple comma keys in the
+		// Test cases where there are multiple colon keys in the
 		// middle.
-		compareTest{"g:a:b", "g:a:b", 0},
-		compareTest{"g:a:b", "g:b:c", -1},
-		compareTest{"g:a:b", "h:a:b", -1},
-		compareTest{"g:a:b:1", "g:a:b:1", 0},
-		compareTest{"g:a:q:1", "h:a:q:1", -1},
-		compareTest{"g:a:b:1", "h:a:b:2", -1},
-		compareTest{"g:a:b:1", "h:a:b:11", -1},
+		comparatorTest{"g:a:b", "g:a:b", 0},
+		comparatorTest{"g:a:b", "g:b:c", -1},
+		comparatorTest{"g:a:b", "h:a:b", -1},
+		comparatorTest{"g:a:b:1", "g:a:b:1", 0},
+		comparatorTest{"g:a:q:1", "h:a:q:1", -1},
+		comparatorTest{"g:a:b:1", "h:a:b:2", -1},
+		comparatorTest{"g:a:b:1", "h:a:b:11", -1},
 	}
 	for _, test := range(tests) {
 		testComparator(t, test, comparer)
 		if test.expectedResult != 0 {
 			// Testing the inverted case
-			invertedTest := compareTest{
+			invertedTest := comparatorTest{
 				test.b,
 				test.a,
 				-test.expectedResult,
